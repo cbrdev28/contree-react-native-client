90c2d983286873804c7f56d0e3fdd29f37ca20e3
Charles-Basile Rouquette <cbrdev28@gmail.com>
2018-10-16 19:28:15 -0700

    Use SocketIo and implement first React App client

--- ContreeClient/App.tsx
+++ ContreeClient/App.tsx
@@ -9,36 +9,71 @@
 import React from "react";
 import { Component } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import SocketIo from "socket.io-client";
 
 interface Props { }
 interface AppState {
-	connected: boolean;
+	status: Status;
+}
+
+enum Status {
+	Disconnected,
+	Connected,
+	Connecting,
 }
 
 export default class App extends Component<Props, AppState> {
-	state = { connected: false };
+	state = { status: Status.Disconnected };
+
+	private URL = "http://10.0.1.80:28028";
+	private socket?: SocketIOClient.Socket = undefined;
+
+	private connectWebSocket = () => {
+		this.socket = SocketIo(this.URL);
+
+		this.socket.on("connect", () => {
+			console.log("Connection opened");
+			this.setState({ status: Status.Connected });
+		});
+		this.socket.on("error", (ev: Event) => {
+			this.setState({ status: Status.Disconnected });
+			console.error("Socket error:", ev);
+		});
+		this.socket.on("disconnect", () => {
+			this.setState({ status: Status.Disconnected });
+			console.warn("Connection closed");
+		});
+	}
 
 	didTapConnect = () => {
-		this.setState({ connected: true });
+		this.setState({ status: Status.Connecting });
+		this.connectWebSocket();
 	}
 
 	didTapDisconnect = () => {
-		this.setState({ connected: false });
+		this.setState({ status: Status.Disconnected });
+		if (this.socket == null) {
+			return;
+		}
+		this.socket.close();
 	}
 
 	render() {
-		const { connected } = this.state;
+		const { status } = this.state;
 		return (
 			<View style={styles.container}>
 				<Text style={styles.welcome}>Welcome to React Native! CBR</Text>
 				<Text style={styles.instructions}>To get started, tap the button below</Text>
-				{connected
-					? <TouchableOpacity onPress={this.didTapDisconnect}>
-						<Text style={styles.welcome}>Disconnect!</Text>
-					</TouchableOpacity>
-					: <TouchableOpacity onPress={this.didTapConnect}>
-						<Text style={styles.welcome}>Connect!</Text>
-					</TouchableOpacity>
+				{
+					status === Status.Connecting
+						? <Text style={styles.instructions}>connecting...</Text>
+						: status === Status.Connected
+							? <TouchableOpacity onPress={this.didTapDisconnect}>
+								<Text style={styles.welcome}>Disconnect!</Text>
+							</TouchableOpacity>
+							: <TouchableOpacity onPress={this.didTapConnect}>
+								<Text style={styles.welcome}>io Connect! {this.URL}</Text>
+							</TouchableOpacity>
 				}
 
 
